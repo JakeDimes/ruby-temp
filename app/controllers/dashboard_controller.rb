8ad004c8c9e99aa4eb9828a573edbc9cd98d6530
JakeDimes <jakebq2001@outlook.com>
2021-11-24 01:04:44 -0500

    Made project compatable with ruby 2.6.6 instead of 2.6.8

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,27 @@
 class DashboardController < ApplicationController
   def admin
+    @account = Account.new
   end
 
   def student
+  end
+
+  def add_account
+
+    # params from the route form submission
+
+    # :account gets account related data from the params
+    account_data = params[:account]
+
+    # create a new account
+    @account = Account.new fname: account_data[:fname], lname: account_data[:lname], dotnum: account_data[:dotnum]
+
+    # save to database
+    # TODO: check .save for success
+    @account.save
+
+    @account = Account.new
+    # render the form page again
+    render 'admin'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'dashboard/admin'
   get 'dashboard/student'
+  post 'dashboard', to: "dashboard#add_account"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
