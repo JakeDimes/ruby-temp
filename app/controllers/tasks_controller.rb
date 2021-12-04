717d8d9ec9fb4414db70082df9a48b923680c96e
TheCooliestGuy <mflavalley@gmail.com>
2021-12-03 20:20:01 -0500

    updated task model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,19 @@
 class TasksController < ApplicationController
-  def new
+  def main
+
+  end
+
+  def delete_task
+
+    # get the team ID
+    name = params[:id]
+
+    # get the team object and delete it
+    Tasks.find(name).delete
+
+    # redirect
+    redirect_to dashboard_admin_tasks_path
+
+
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
+  has_many :accounts
+  has_many :teams, through: :gets_assigned
 
+  validates :name, presence: true
+  validates :team_id, presence: true
+  validates :due_date, presence: true
+  validates :status, presence: true
 end
