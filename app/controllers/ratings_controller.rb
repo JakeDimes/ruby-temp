06ee2c8459e46c1bfec1f21b1bd8c5deae868895
TheCooliestGuy <mflavalley@gmail.com>
2021-12-03 23:46:49 -0500

    created home page and looked at routing, routing still needs fixed

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,4 +1,21 @@
 class RatingsController < ApplicationController
   def new
+
   end
+
+  def delete_rating
+
+    # get the team ID
+    eval_id = params[:id]
+
+    # get the team object and delete it
+    Rating.find(eval_id).delete
+
+    # redirect
+    redirect_to dashboard_admin_ratings_path
+
+
+  end
+
+
 end
