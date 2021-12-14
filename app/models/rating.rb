d7941af3cf4fbf76947e4ae038200052a017e26d
BBehr960 <behr.30@osu.edu>
2021-12-14 00:38:23 -0500

    comments and validations

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,8 +1,10 @@
 class Rating < ApplicationRecord
-  # has_many :tasks
-  #
-  # validates :eval_id, presence: true
-  # validates :team_id, presence: true
-  # validates :due_date, presence: true
-  # validates :status, presence: true
+  #student who is rating id must be an integer and present
+  validates :rating_student , presence: true
+  #student who is getting rated id must be integer and present
+  validates :rated_student, presence: true
+  #the rating must be an integer from 0-10
+  validates :rating, presence: true, numericality: {only_integer: true, message: "Number must be an Integer from 0-10"}
+  #comment must be present
+  validates :comment, presence: true
 end
